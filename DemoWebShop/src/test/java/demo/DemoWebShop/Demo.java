f1103b721c89ba9d9f94bbc9abe79c06c7d42387
Dell <Dell@192.168.1.11>
2023-09-06 10:19:47 +0530

    I develop a new Program

---
+++ DemoWebShop/src/test/java/demo/DemoWebShop/Demo.java
@@ -0,0 +1,18 @@
+package demo.DemoWebShop;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
